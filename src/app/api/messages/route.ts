5ae914bf956d0108cb0cacacb9c755a70b595408
Anish <xevenbiswas@gmail.com>
2023-12-09 13:03:46 +0530

    fixed

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,12 +1,13 @@
 import prisma from "@/lib/db/prisma";
-import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    const { content ,userId} = body;
-    console.log("body :", content, userId);
+    const { content, userId } = body;
+    console.log("body:", content, userId);
+    
     if (userId) {
       const message = await prisma.message.create({
         data: {
@@ -16,10 +17,10 @@ export async function POST(req: Request) {
       });
 
       console.log(message);
-      return Response.json({ message }, { status: 201 });
+      return new NextResponse(JSON.stringify({ message }), { status: 201 });
     }
   } catch (error) {
     console.log(error);
-    return { status: 500, body: { error: "Internal server error" } };
+    return new NextResponse(JSON.stringify({ error: "Internal server error" }), { status: 500 });
   }
 }
