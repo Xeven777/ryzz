f1d9f8fb619b226e3b1fb9cee097e506b338cc0b
Anish <xevenbiswas@gmail.com>
2023-12-09 14:29:16 +0530

    chnged

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/send/*"],
+  publicRoutes: ["/", "/send/"],
 });
 
 export const config = {
