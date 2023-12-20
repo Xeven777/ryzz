c9a135bd6c473d6e047ba505b33a4329e2796914
Anish <xevenbiswas@gmail.com>
2023-12-20 18:17:56 +0530

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,28 @@ import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "Ryzz",
   description: "Send Anonymous Messages to your Friends!",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://ryzz.vercel.app",
+    images: [
+      {
+        url: "https://i.imgur.com/22Bvx0El.png",
+        width: 1200,
+        height: 630,
+        alt: "Ryzz",
+      },
+    ],
+  },
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -21,7 +39,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body >{children}</body>
+        <body>{children}</body>
         <Toaster />
       </html>
     </ClerkProvider>
