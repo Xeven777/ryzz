7e12448711ffd78afea6db7c5beefc1abb987e00
Anish <xevenbiswas@gmail.com>
2023-12-20 21:49:21 +0530

    space

--- src/components/Wap.tsx
+++ src/components/Wap.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Wap = ({ linkprop }: Props) => {
   return (
     <a
-      href={`https://wa.me/?text=Hey!%20Send%20me%20anonymous%20messages%20in%20Ryzz%20.%20I%20will%20never%20know%20who%20had%20send%20it%20XD%20https://${linkprop}`}
+      href={`https://wa.me/?text=Hey!%20Send%20me%20anonymous%20messages%20in%20Ryzz%20.%20I%20will%20never%20know%20who%20had%20send%20it%20XD%20%20${linkprop}`}
       target="_blank"
       className="flex gap-2 text-sm h-10 items-center hover:bg-green-500 active:bg-green-500 transition-all bg-green-600 text-white px-3 rounded-md"
     >
