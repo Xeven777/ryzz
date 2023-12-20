33d9bb4db3db8ddfbc1dd5279011768c306e4208
Anish <xevenbiswas@gmail.com>
2023-12-20 21:42:32 +0530

    links

--- src/components/Wap.tsx
+++ src/components/Wap.tsx
@@ -1,13 +1,13 @@
 import { Share2Icon } from "lucide-react";
 
 interface Props {
-    linkprop: string;
+  linkprop: string;
 }
 
-const Wap = ({linkprop}:Props) => {
+const Wap = ({ linkprop }: Props) => {
   return (
     <a
-      href={`https://wa.me/?text=Hey!%20I%20am%20using%20Ryzz%20to%20send%20messages%20anonymously.%20Join%20me%20here%20https://${linkprop}`}
+      href={`https://wa.me/?text=Hey!%20Send%20me%20anonymous%20messages%20in%20Ryzz%20.%20I%20will%20never%20know%20who%20had%20send%20it%20XD%20https://${linkprop}`}
       target="_blank"
       className="flex gap-2 text-sm h-10 items-center hover:bg-green-500 active:bg-green-500 transition-all bg-green-600 text-white px-3 rounded-md"
     >
